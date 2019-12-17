2fc9f25c963b0921e11e43ddcf03122d763ef2d7
pseuyi <pseuyi@gmail.com>
2019-12-17 14:07:02 -0800

    add solution for day 8 part 1

---
+++ day-8-part-1.ts
@@ -0,0 +1,64 @@
+var fs = require('fs');
+
+fs.readFile('test.txt', function(err: any, data: any) {
+  if (err) throw err;
+
+  const input = data
+    .toString()
+    .split('')
+    .filter((s: string) => s.length > 0 && s !== '\n')
+    .map((n: string) => parseInt(n, 10));
+
+  decodeImage(input);
+});
+
+type Layer = number[][];
+
+const WIDTH = 25;
+const HEIGHT = 6;
+
+const decodeImage = (input: number[]) => {
+  const layers: Layer[] = [];
+
+  let layer: Layer = [];
+  let row: number[] = [];
+
+  // divide the input into sets of width
+  for (let i = 0; i < input.length; i++) {
+    const digit = input[i];
+    row.push(digit);
+
+    if (row.length == WIDTH) {
+      layer.push(row);
+      row = [];
+    }
+
+    if (layer.length == HEIGHT) {
+      layers.push(layer);
+      layer = [];
+    }
+  }
+
+  const zeroCount: number[] = layers.map(layer => countNums(layer, 0));
+
+  const leastZeros = Math.min(...zeroCount);
+  const leastZeroIdx = zeroCount.indexOf(leastZeros)
+  const leastZeroLayer = layers[leastZeroIdx] 
+
+  const result = countNums(leastZeroLayer, 1) * countNums(leastZeroLayer, 2)
+  
+  console.log('result: ', result)
+  return result
+};
+
+const countNums = (layer: Layer, n: number): number => {
+  let count = 0;
+
+  for (const row of layer) {
+    for (const digit of row) {
+      if (digit == n) count++;
+    }
+  }
+
+  return count;
+};
